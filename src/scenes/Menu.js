3db9ea32b0d9700f8beceb6569677a815dcfc67b
Cloid <nintendogamer@live.com>
2020-06-06 22:23:19 -0700

    Updated to start on Start Level

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -49,7 +49,7 @@ class Menu extends Phaser.Scene {
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyM)) {
-            this.scene.start('Intro1');       
+            this.scene.start('Start');       
         }
         
     }
